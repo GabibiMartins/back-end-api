8beb674cf917a3d7c552a840203247edcc0ec134
GabibiMartins <gmmg@aluno.ifnmg.edu.br>
2025-09-18 17:47:40 +0000

    BD

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+import pkg from "pg";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+app.get("/", async (req, res) => {        
   console.log("Rota GET / solicitada");
+
+  const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
   res.json({
 		message: "API para _____",      // Substitua pelo conteúdo da sua API
     author: "Gabriela Martins Matos Gomes",    // Substitua pelo seu nome
+    statusBD: dbStatus
   });
 });
 
